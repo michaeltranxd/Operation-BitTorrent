27f753cadada9c7dd2189bb4d660bc5d77539f19
Jason Gu <35580491+Wipeitquick@users.noreply.github.com>
2018-03-10 01:46:38 -0600

    Update file_transfer.c

--- server/file_transfer.c
+++ server/file_transfer.c
@@ -1,21 +1,22 @@
-
 int send_file(char* filename, int new_fd){
 	FILE* fp = fopen(filename, "r");
 	if (!fp) {
 		printf("Fail to open file %s\n", filename);
-		continue;
+		exit(0);
 	}
 	
 	int total_bytes_send = 0;
 	while (1) {
 		unsigned char buff[256] = {0};
-		memeset(buff, '\0', sizeof(buff));
+		memset(buff, '\0', sizeof(buff));
 		int bytes_read = fread(buff, sizeof(char), 256, fp);
 
 		if (bytes_read > 0) {
 			printf("Successfully read %d bytes, now send \n", bytes_read);
-			if (write(new_fd, buff, bytes_read) == -1)
+			if (write(new_fd, buff, bytes_read) == -1){
 				printf("Error writing to new_fd %d\n", new_fd);
+				exit(0);
+			}
 			total_bytes_send += bytes_read;
 		}
 
@@ -26,12 +27,13 @@ int send_file(char* filename, int new_fd){
 
 		if (ferror(fp)) {
 			printf("Error reading file, total_bytes_send is %d\n", total_bytes_send);
-			break;
+			exit(0);
 		}
 	}
 	fclose(fp);
 	return total_bytes_send;
 }
+
 
 
 
@@ -44,8 +46,9 @@ int recv_file(char* filename, int sockfd) {
 	while (1) {
 		bytes_recv = read(sockfd, buff, 256);
 		if (bytes_recv < 0) {
-			printf("Read error\n");
-			break;
+			perror("Read error\n");
+			fclose(fp);
+			exit(0);
 		}
 		else {
 			printf("Successfully receive %d bytes\n", bytes_recv);
